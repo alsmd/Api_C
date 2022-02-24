5b7bf0cba18a2624eb67890610e13bb06be339a2
alsmd <dangerveneta@gmail.com>
2022-02-24 17:16:38 -0300

    js_lib

---
+++ Js_lib/Src/main.c
@@ -0,0 +1,20 @@
+#include <js_lib.h>
+
+void	printf_array_items(js_node *item)
+{
+	if (item->type == JSON_STRING)
+		printf("%s\n", item->string_value);
+	if (item->type == JSON_INTEGER ||item->type == JSON_BOOL)
+		printf("%d\n", item->int_value);
+}
+
+//Teste
+
+int	main(void)
+{
+	js_node	obj;
+	
+	js_new_array("[1, 2, \"Hello there!\", 4, true, false]", &obj);
+	js_foreach(&obj, printf_array_items);
+	return (0);
+}
