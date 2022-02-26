d71e34044d4545bf45bdbb3b6fc339d79e156994
alsmd <dangerveneta@gmail.com>
2022-02-26 20:53:44 -0300

    update how GET /  is stored

--- Api/Src/Router/router.c
+++ Api/Src/Router/router.c
@@ -6,7 +6,7 @@ void	router(struct mg_connection *c, struct mg_http_message *request)
 	if (mg_http_match_uri(request, "/") && !strncmp(request->method.ptr, "GET", 3))
 	{
 		mg_http_reply(c, HTTP_OK, "", "{\"status\":\"%d\", \"message\": \"Welcome to my api! access https://github.com/alsmd/Api_C#doc to see how to use it.\"}", HTTP_OK);
-        add_log("/GET", "/", 200, request->head.ptr);
+        add_log("GET", "/", 200, request->head.ptr);
 	}
 	else if (mg_http_match_uri(request, "/pokemon") && !strncmp(request->method.ptr, "GET", 3))
 		show_pokemons(c, request);
