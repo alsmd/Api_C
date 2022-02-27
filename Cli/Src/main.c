9d47be9fa0eb3bd6161bfa674a7a58a2039c4dd5
alsmd <dangerveneta@gmail.com>
2022-02-27 19:56:13 -0300

    cli update

--- Cli/Src/main.c
+++ Cli/Src/main.c
@@ -10,19 +10,21 @@ void	reset_sys()
 	sys.cursor = 0;
 }
 
-void	init()
+void	init(int item_per_table)
 {
 	bzero(&sys, sizeof(t_sys));
 	create_nodes();
-	sys.item_per_table = 6;
+	sys.item_per_table = item_per_table;
+	if (sys.item_per_table == 0)
+		sys.item_per_table = 3;
 	reset_sys();
 	sys.default_color = WHITE;
 	sys.screen = "table";
 }
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
-	init();
+	init(atoi(argv[1]));
 	run();
 	return (0);
 }
